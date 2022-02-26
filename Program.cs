b4a84a283fab083db9a8baa48c26038cfbdad2f9
Brandon Belger <bbelger@wctc.edu>
2022-02-26 13:18:01 -0600

    added nlog and ticket path directory

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-
+using NLog.Web;
+using System.IO;
 namespace TicketSystemClass
 {
     class Program
     {
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
